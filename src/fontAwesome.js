7ba2e4f0dcb08aaf86e5b8cfee2b6ed099d1d0a9
aderose <sandro.derose28@yahoo.com>
2021-02-18 18:06:41 +0000

    Initialise fontAwesome icons

---
+++ src/fontAwesome.js
@@ -0,0 +1,8 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBookOpen);
+library.add(faSignOutAlt);
+
+export default FontAwesomeIcon;
