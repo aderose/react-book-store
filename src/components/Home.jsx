4046f23ba2dbda6c8b8eb1bbed4ed8ca11144b21
aderose <sandro.derose28@yahoo.com>
2021-02-24 14:36:30 +0000

    Add Book Lists to to the home page component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
 
+import { bought, notBought } from '../books.json';
+
 import Logo from './Logo';
 import Profile from './Profile';
 
 import { GlobalStyle, Header } from '../style';
 
 import { UserContext } from '../providers/UserProvider';
+import BookList from './BookList';
 
 const Home = () => {
-  const user = useContext(UserContext);
-  const { photoURL, displayName } = user;
-  console.log(user.photoURL);
+  const { photoURL, displayName } = useContext(UserContext);
   return (
     <React.Fragment>
       <GlobalStyle />
@@ -18,6 +19,8 @@ const Home = () => {
         <Logo />
         <Profile iconSrc={photoURL} name={displayName} />
       </Header>
+      <BookList content="bought" books={bought} />
+      <BookList content="not bought" books={notBought} />
     </React.Fragment>
   );
 };
