fbf8b0b6e47f039729bc8357a216344a87930ef4
aderose <sandro.derose28@yahoo.com>
2021-03-07 19:59:12 +0000

    Add home component with Mui styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-import { bought, notBought } from '../books.json';
+import { books } from '../books.json';
 
-import Logo from './Logo';
-import Profile from './Profile';
-
-import { GlobalStyle, Header } from '../style';
-
-import { UserContext } from '../providers/UserProvider';
 import BookList from './BookList';
+import NavBar from './NavBar';
 
 const Home = () => {
-  const { photoURL, displayName } = useContext(UserContext);
   return (
     <React.Fragment>
-      <GlobalStyle />
-      <Header>
-        <Logo />
-        <Profile iconSrc={photoURL} name={displayName} />
-      </Header>
-      <BookList content="bought" books={bought} />
-      <BookList content="not bought" books={notBought} />
+      <NavBar />
+      <BookList books={books} />
     </React.Fragment>
   );
 };
