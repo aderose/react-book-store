f35addc6c5cb35b264ea80281966606774e62a18
aderose <35845363+aderose@users.noreply.github.com>
2021-03-11 14:29:21 +0000

    Develop (#6)
    * Isolate FontAwesome to separate module
    
    * Initialise fontAwesome icons
    
    * Implement and export the signOut function
    
    * Implement the profile to show userf information when logged in
    
    * Add the profile component to the home header
    
    * Style the profile component
    
    * Add initial test books
    
    * Adjust logo style
    
    * Create styled components for books and lists
    
    * Create a book component for book objects
    
    * Create a list of books passed through props
    
    * Include addition FA icons
    
    * Add Book Lists to to the home page component
    
    * Import material ui and remove styled & fontawesome
    
    * Update to material fonts
    
    * Adjust default book format
    
    * Add material ui styles
    
    * Remove styled components
    
    * Add Css Baseline & theme for global styles
    
    * Update logo styling
    
    * Update login component to utilise Material ui
    
    * Update profile component to utilise Material ui
    
    * Update Book to use material ui styling
    
    * Update booklist to Mui styling & new structure
    
    * Add navbar using Mui styling
    
    * Add home component with Mui styling
    
    * Configure firebase cloud firestore
    
    * Handle firestore document creation & reads
    
    * Remove prop funnelling of books
    
    * Create book factory function
    
    * Remove IDs from initial books
    
    * Safe interface to firebase & cache bookList
    
    * Use bookList manager to populate booklist
    
    * Avoid undefined object error
    
    * Adjust BookListManager object to a custom hook
    
    * Remove unnecessary object definition
    
    * Replace BookListManager object definition with custom hook
    
    * Utilise BookListManager hook to manage list
    
    * Import formik to manage add/edit form
    
    * Add styling adjustments
    
    * Implement dummy card to add new book & include form
    
    * Create book form using formik & material ui
    
    * Correct price formatting
    
    * Remove fontAwesome config
    
    * Correct form background not spreading the whole height
    
    * Add some bottom border to card chips

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,29 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { DummyCard, TransparentPaper } from '../styles';
 
 import useBookListManager from '../hooks/useBooksManager';
 
-import { TransparentPaper } from '../styles';
+import AddIcon from '@material-ui/icons/Add';
 
+import BookForm from './BookForm';
 import Book from './Book';
 
 const BookList = () => {
-  const { books, initializeBooks } = useBookListManager();
+  const { books, initializeBooks, addBook } = useBookListManager();
+  const [formActive, setFormActive] = useState(false);
 
   useEffect(() => {
     initializeBooks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const toggleForm = () => {
+    setFormActive((prevStatus) => !prevStatus);
+  };
+
   return (
-    <Container maxWidth="md">
-      <TransparentPaper elevation={0} square>
-        {books.map((book, index) => (
-          <Book key={index} book={book} />
-        ))}
-        <Book book={{ id: 'DUMMY' }} />
-      </TransparentPaper>
-    </Container>
+    <React.Fragment>
+      <Container maxWidth="md">
+        {formActive ? (
+          <BookForm addBook={addBook} hideForm={toggleForm} />
+        ) : (
+          <TransparentPaper elevation={0} square>
+            <DummyCard type="button" onClick={toggleForm}>
+              <AddIcon />
+              <Typography variant="h5" component="p">
+                Add Book
+              </Typography>
+            </DummyCard>
+            {books.map((book, index) => (
+              <Book key={index} book={book} />
+            ))}
+          </TransparentPaper>
+        )}
+      </Container>
+    </React.Fragment>
   );
 };
 
