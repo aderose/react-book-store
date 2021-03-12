9b864f83321f9cfce5889f80189d95b68a25f3e2
aderose <sandro.derose28@yahoo.com>
2021-03-12 15:25:28 +0000

    Use then structure to get document id

--- src/firebase.js
+++ src/firebase.js
@@ -46,7 +46,7 @@ export const signOut = () => {
 };
 
 export const addBookToFirestore = async (book) => {
-  const docRef = await firestore
+  return await firestore
     .collection('users')
     .doc(auth.currentUser.uid)
     .collection('books')
@@ -56,10 +56,10 @@ export const addBookToFirestore = async (book) => {
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
     })
+    .then((docRef) => docRef.id)
     .catch((e) => {
       console.error('Error writing new book to database', e);
     });
-  return docRef.id;
 };
 
 export const getBooksFromFirestore = async () => {
