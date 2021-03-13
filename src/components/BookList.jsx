a48561b04cb6a8597b8b99e33b3b32aa0f28f542
aderose <sandro.derose28@yahoo.com>
2021-03-13 16:30:50 +0000

    Remove dummy components, initialize books on mount

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,76 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import { DummyCard, TransparentPaper } from '../styles';
+import React, { useEffect } from 'react';
 
 import useBooksManager from '../hooks/useBooksManager';
 
-import AddIcon from '@material-ui/icons/Add';
-
-import BookForm from './BookForm';
 import Book from './Book';
 
-const BookList = () => {
+const BookList = ({ toggleEditForm }) => {
   const {
     books,
     initializeBooks,
-    addBook,
     updateBookProperty,
-    updateBook,
     removeBook,
   } = useBooksManager();
-  const [formActive, setFormActive] = useState(false);
-  const [editBook, setEditBook] = useState(undefined);
 
   useEffect(() => {
-    initializeBooks();
+    initializeBooks().then(() => {
+      console.log('Got books from Firestore (BookList)');
+      console.log({ booksLength: books.length });
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const toggleForm = () => {
-    setFormActive((prevStatus) => !prevStatus);
-  };
-
-  const toggleEditForm = (book) => () => {
-    setEditBook(book);
-    toggleForm();
-  };
-
-  const hideForm = () => {
-    setEditBook(undefined);
-    toggleForm();
-  };
-
-  return (
-    <Container maxWidth="md">
-      {formActive ? (
-        <BookForm
-          addBook={addBook}
-          updateBook={updateBook}
-          hideForm={hideForm}
-          editBook={editBook}
-        />
-      ) : (
-        <TransparentPaper elevation={0} square>
-          <DummyCard type="button" onClick={toggleForm}>
-            <AddIcon />
-            <Typography variant="h5" component="p">
-              Add Book
-            </Typography>
-          </DummyCard>
-          {books.map((book) => (
-            <Book
-              key={book.id}
-              book={book}
-              updateBookProperty={updateBookProperty}
-              showForm={toggleEditForm(book)}
-              removeBook={removeBook}
-            />
-          ))}
-        </TransparentPaper>
-      )}
-    </Container>
-  );
+  return books.map((book) => (
+    <Book
+      key={book.id}
+      book={book}
+      updateBookProperty={updateBookProperty}
+      showForm={toggleEditForm(book)}
+      removeBook={removeBook}
+    />
+  ));
 };
 
 export default BookList;
