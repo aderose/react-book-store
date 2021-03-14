b92933bd886f9bb37202826e2b2573fc2aecd166
aderose <sandro.derose28@yahoo.com>
2021-03-14 15:00:27 +0000

    implement useFirebase hook for auth

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, createContext } from 'react';
-import { auth } from '../firebase';
+import useFirebase from '../hooks/useFirebase';
 
 export const UserContext = createContext({ user: null });
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const { auth } = useFirebase();
 
   useEffect(() => {
     auth.onAuthStateChanged((userAuth) => {
