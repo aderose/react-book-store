446ef329f5fcb20e7e7def6fad918195a4a4cee2
aderose <sandro.derose28@yahoo.com>
2021-03-24 22:41:54 +0000

    Style using tailwind & implement FadePopup wrapper

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,65 +1,41 @@
 import React, { useState, useContext, useRef } from 'react';
-import Typography from '@material-ui/core/Typography';
-import Fade from '@material-ui/core/Fade';
-import Popper from '@material-ui/core/Popper';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Button from '@material-ui/core/Button';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Box from '@material-ui/core/Box';
-
-import { StyledProfileCard, ProfileIcon } from '../styles';
 import { UserContext } from '../providers/UserProvider';
+import FadePopup from './FadePopup';
 
 const Profile = ({ firebase }) => {
   const { displayName, email } = useContext(UserContext);
   const [open, setOpen] = useState(false);
-  const anchorRef = useRef(null);
+  const positionRef = useRef(null);
   const { signOut } = firebase;
 
-  const handleToggle = () => setOpen((prevOpen) => !prevOpen);
-
-  const handleClose = (event) => {
-    // if click occurs on anchor, prevent rapid open & close
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-
-    setOpen(false);
-  };
+  const togglePopup = () => setOpen((prevOpen) => !prevOpen);
+  const hidePopup = () => setOpen(false);
 
   return (
     <React.Fragment>
-      <ProfileIcon type="button" ref={anchorRef} onClick={handleToggle}>
-        {displayName[0].toUpperCase()}
-      </ProfileIcon>
-      <Popper open={open} anchorEl={anchorRef.current} transition>
-        {({ TransitionProps }) => (
-          <Fade {...TransitionProps}>
-            <Box>
-              <ClickAwayListener onClickAway={handleClose}>
-                <StyledProfileCard elevation={5}>
-                  <CardContent>
-                    <Typography variant="h5" component="h2" gutterBottom>
-                      {displayName}
-                    </Typography>
-                    <Typography
-                      variant="body1"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      {email}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button onClick={signOut}>Sign Out</Button>
-                  </CardActions>
-                </StyledProfileCard>
-              </ClickAwayListener>
-            </Box>
-          </Fade>
-        )}
-      </Popper>
+      <button
+        ref={positionRef}
+        className="w-10 h-10 leading-10 text-2xl uppercase text-gray-100 bg-strong-purple hover:bg-purple-800 rounded-full text-center transition duration-200 ease-in"
+        onClick={togglePopup}
+      >
+        {displayName[0]}
+      </button>
+      <FadePopup
+        id="profile-card"
+        show={open}
+        hide={hidePopup}
+        parent={positionRef.current}
+        classes="bg-gray-100 rounded shadow-lg z-10 p-4 border"
+      >
+        <p className="text-2xl">{displayName}</p>
+        <p className="mt-2 text-gray-500">{email}</p>
+        <button
+          className="mt-6 -ml-2 p-2 rounded transition-colors duration-200 ease-in text-gray-700 uppercase tracking-wide text-sm font-semibold hover:bg-gray-200"
+          onClick={signOut}
+        >
+          Sign Out
+        </button>
+      </FadePopup>
     </React.Fragment>
   );
 };
