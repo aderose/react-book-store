1c795aa99bd6e18272aeda081c53e652a38714ce
aderose <sandro.derose28@yahoo.com>
2021-03-25 17:26:38 +0000

    Add container to wrap card header to prevent unwanted wrapping

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,19 +4,21 @@ import Chip from './Chip';
 
 const Book = ({ book, updateBookProperty, showForm, removeBook }) => {
   return (
-    <article className="flex flex-wrap items-center justify-between m-3 p-4 w-56 bg-gray-100 rounded shadow-lg">
-      <p className="basis italic text-gray-500">{book.author}</p>
-      <BookMenu
-        book={book}
-        switchReadStatus={() =>
-          updateBookProperty(book.id, 'isRead', !book.isRead)
-        }
-        switchBoughtStatus={() =>
-          updateBookProperty(book.id, 'isBought', !book.isBought)
-        }
-        handleEdit={() => showForm()}
-        handleDelete={() => removeBook(book.id)}
-      />
+    <article className="m-3 p-4 w-56 bg-gray-100 rounded shadow-lg">
+      <div className="flex justify-between items-center">
+        <p className="italic text-gray-500">{book.author}</p>
+        <BookMenu
+          book={book}
+          switchReadStatus={() =>
+            updateBookProperty(book.id, 'isRead', !book.isRead)
+          }
+          switchBoughtStatus={() =>
+            updateBookProperty(book.id, 'isBought', !book.isBought)
+          }
+          handleEdit={() => showForm()}
+          handleDelete={() => removeBook(book.id)}
+        />
+      </div>
       <h1 className="mt-2 font-semibold text-lg">{book.title}</h1>
       <div className="mt-2 flex flex-wrap">
         {book.price && (
